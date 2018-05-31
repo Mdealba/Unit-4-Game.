59cb96622b4acf8856c09a03e105b1d3b6566d1c
Mdealba <M_dealba@yahoo.com>
2018-05-31 16:50:07 -0700

    added style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,84 +1,107 @@
-// Generate a Randome Number between 9-120
-
-// Generate a Randome number and assing them to the crystal between 1-12
-
-// Keep track of the players Score
-
-// check if the play won or lost
-
-var taco = {
-    asada: {
-        name: "asada",
-        value: 0,
-    },
-    alpastor: {
-        name: "alpastor",
-        value: 0,
-    },
-    carnitas: {
-        name: "carnitas",
-        value: 0,
-    },
-    chorizo: {
-        name: "chorizo",
-        value: 0,
-    },
+// create a hash that stores all the global variables
+var data = {
+    wins:0,
+    losses:0,
+    target: numberGenerator(19, 120),
+    total:0,
+    tacos: []
 }
-var targetScore = 0;
-var playerScore = 0;
-var wins = 0;
-var loses = 0;
 
-
-var randomNumber = function (min, max) {
-   return Math.floor(Math.random() * (max - min + 1)) + min
+// create a function that generates a random number between x-y
+function numberGenerator(min, max){
+    return Math.floor(Math.random()*(max-min+1)+1);
 }
-var startGame = function () {
-    playerScore = 0;
-    targetScore = randomNumber(9, 120)
-    taco.asada.value = randomNumber(1, 12)
-    taco.alpastor.value = randomNumber(1, 12)
-    taco.carnitas.value = randomNumber(1, 12)
-    taco.chorizo.value = randomNumber(1, 12)
 
-    $("#target-score").text(targetScore)
-    $("#player-score").text(playerScore)
-    console.log("targetScore", targetScore)
-    console.log("asada", taco.asada.value)
-    console.log("alpastor", taco.alpastor.value)
-    console.log("carnitas", taco.carnitas.value)
-    console.log("chorizo", taco.chorizo.value)
-}
-var checkWin = function () {
-    targetScore === playerScore 
-    if (targetScore === playerScore) {
-        wins++
-        $("#Wins").text(wins)
-        startGame()
-    }
-    else if (targetScore < playerScore) {
-        losses++
-        $("#Losses").text(losses)
-        startGame()
+// assign 4 values to an array
+function assignGemValues(){
+    while (data.tacos.length < 5) {
+        var num = numberGenerator(1, 12);
+        if(!data.tacos.includes(num)){
+            data.tacos.push(num);
+        }
     }
 }
+// initaial value
+assignGemValues();
 
-var addValues = function(clickedTaco){
-    playerScore += clickedTaco.value
-    $("#player-score").text(playerScore)
-    checkWin()
+
+function displayHTML(){
+    $("#wins").text(data.wins);
+    $("#losses").text(data.losses);
+    $("#target").text(data.target);
+    $("#total").text(data.total);
+}
+// display initaial value
+displayHTML();
+
+// create a click event for btns
+$(".button").on("click", function(){
+    console.log("clicked");
+    $("#message").text("");
+    var selectedID =$(this).attr("id");
+    // add valut to total
+    // check total
+    switch(selectedID){
+        case "btn1":
+            data.total = data.total + data.tacos[0];
+            $("#total").text(data.total);
+            if(data.total> data.target){
+                handleLosses();
+            }else if(data.total === data.target){
+                handleWins();
+            }
+        break;
+        case "btn2":
+        data.total = data.total + data.tacos[1];
+        $("#total").text(data.total);
+        if(data.total> data.target){
+            handleLosses();
+        }else if(data.total === data.target){
+            handleWins();
+        }
+    break;
+    case "btn3":
+    data.total = data.total + data.tacos[2];
+    $("#total").text(data.total);
+    if(data.total> data.target){
+        handleLosses();
+    }else if(data.total === data.target){
+        handleWins();
+    }
+break;
+case "btn4":
+data.total = data.total + data.tacos[3];
+$("#total").text(data.total);
+if(data.total> data.target){
+    handleLosses();
+}else if(data.total === data.target){
+    handleWins();
+}
+break;
+
+    }
+});
+
+function handleLosses(){
+    data.losses++;
+    $("#message").text("you lose!");
+    resetGame();
+}
+function handleWins(){
+    data.wins++;
+    $("#message").text("you win!");
+    resetGame();
+}
+function resetGame(){
+    data.target =  numberGenerator(19, 120);
+    data.total = 0;
+    data.tacos= [];
+    assignGemValues();
+    displayHTML();
 }
 
-startGame()
-$("#asada").click(function(){
-    addValues(taco.asada)
-})
-$("#alpastor").click(function(){
-    addValues(taco.alpastor)
-})
-$("#carnitas").click(function(){
-    addValues(taco.carnitas)
-})
-$("#chorizo").click(function(){
-    addValues(taco.chorizo)
-})
+// handles wins and losses
+
+// function that handles wins
+
+// function that handles losses
